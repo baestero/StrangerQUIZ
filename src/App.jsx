a1dceb6cb4215fe3a5a433c81ce539bb0cbf6c5b
baestero <leonardobaestero@gmail.com>
2025-08-05 17:41:53 -0300

    atualização

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./app.module.css";
+import styles from "./styles/app.module.css";
 import logo from "/Stranger_Things_logo.png";
 
 const perguntas = [
